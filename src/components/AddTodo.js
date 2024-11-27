df0b5d6af0ece584bc419334f10d3e60d221b3de
Le Hoang Nam <lehoangnam.31123@gmail.com>
2024-11-27 15:46:22 +0700

    refactor(AddTodo + TodoApp): Write comments for code

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -3,10 +3,12 @@ import React, { useState, useEffect } from "react";
 function AddTodo(props) {
     const [title, setTitle] = useState("")
     
+    // Xử lý cập nhật giá trị cho field input
     const onInputChange = e => {
         setTitle(e.target.value)
     }
 
+    // Xử lý lấy props được truyền từ TodoApp để hoàn thành thao tác thêm mới todo
     const addToDo = e => {
         e.preventDefault();
         props.addToDo(title);
